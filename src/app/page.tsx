7252ddbe92756ae19a4ed943e6ae2d92f0106788
tojo-joseph <tojo.joseph18@gmail.com>
2024-08-11 20:14:54 +0530

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { ChartCard } from "@/components/chart-card/chart-card";
-
 export default function HomePage() {
   return (
     <div>
